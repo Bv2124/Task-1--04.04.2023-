1618e0c71092693f4ba3e2defe3b6f88c19b023d
Bv2124 <115614444+Bv2124@users.noreply.github.com>
2023-04-04 21:52:34 +0530

    04/04/2023
    A Navigation task

---
+++ App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Navbar from './Navbar';
+import Sec1 from './Sec1';
+import Title from './Title';
+
+function App() {
+  return (
+    <div className='Monday'>
+    <Title/>
+    <Navbar/>
+    <Sec1/>
+        </div>
+  );
+}
+
+export default App;
---
+++ Title.js
@@ -0,0 +1,14 @@
+export default function Title(){
+    return(
+    <div className="full-paragraph-component">
+        <div className="full-paragraph-wrapper" style={{maxWidth:'1100px'}}>
+            <div className="full-paragraph align-items-center" style={{textAlign:'center'}}>
+                <div className="title-wrapper" style={{margintop:'0px'}}>
+                <h2 class="core-title lg" style={{fontSize:'45px'}}><b>Everything</b><light> you need for </light><b>any workflow</b></h2>
+                </div>
+                <div class=" paragraph-body-wrapper"><div class="paragraph-body md">Easily build your ideal workflow with monday.com building blocks.</div></div>
+            </div>
+        </div>
+    </div>
+    )
+}
